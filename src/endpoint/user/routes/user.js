270fe490b025f6cb30231f0295d3e07e89e314cc
ZamIzzudin <88259831+ZamIzzudin@users.noreply.github.com>
2023-06-20 18:50:41 +0700

    give access to user can update profile

--- src/endpoint/user/routes/user.js
+++ src/endpoint/user/routes/user.js
@@ -1,12 +1,12 @@
 import express from "express"
 import controller from '../controllers/user.js'
-import { admins } from '../../../middeware/access_auth.js'
+import { admins, is_login} from '../../../middeware/access_auth.js'
 
 const user = express.Router()
 
 user.get('/', admins, controller.user_list)
 
-user.put('/:id_user', admins, controller.update_user)
+user.put('/:id_user', is_login, controller.update_user)
 user.delete('/:id_user', admins, controller.delete_user)
 
 
